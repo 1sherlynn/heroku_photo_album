968a7dcf3569871c7aa73c0a99cad13ec2708ca4
Sherlynn Tan <1sherlynn@gmail.com>
2017-10-25 01:27:24 +0800

    updated to unclude dependent destroy for relevant models

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ApplicationRecord
-	has_many :photos
+	has_many :photos, dependent: :destroy 
 	belongs_to :user
 
 	mount_uploader :avatar, AvatarUploader
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :albums
-  has_many :comments
+  has_many :albums, dependent: :destroy
+  has_many :comments, dependent: :destroy 
   
 end
